afed6e80b9ebb487184f6d3c52b6dece66ab9995
git <smendoza.barrera@gmail.com>
2015-10-21 20:43:31 -0500

    In the beginning there was darkness v0.0.1

---
+++ javascripts/equalheights-init.js
@@ -0,0 +1,54 @@
+/*global $:false */
+/*global window: false */
+
+(function(){
+  "use strict";
+
+
+$(function ($) {
+
+
+$(window).load(function(){
+    
+
+        //EqualHeights triggering
+		    $('.equal-height').setAllToMaxHeight();
+		    $('.about-content-inner').setAllToMaxHeight();
+		    $('.service-item').setAllToMaxHeight();
+		    $('.team-info, .team-thumb').setAllToMaxHeight();
+		    $('.contact-content-inner').setAllToMaxHeight();
+		    $('.footer-column').setAllToMaxHeight();
+		    $('.about-content').setAllToMaxHeight();
+		    $('.news-post-inner').setAllToMaxHeight();
+		    $('.error-content-inner').setAllToMaxHeight();
+   
+
+
+    });
+			
+		   
+						
+		
+        
+   
+});
+// $(function ($)  : ends
+
+})();
+//  JSHint wrapper $(function ($)  : ends
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+  
+
